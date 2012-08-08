7facfd305afea18c63ce12a25640a66e59ce0014
Vishnu Menon <vishthemenon@gmail.com>
2012-08-08 23:45:15 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VultureWeb::Application.config.secret_token = 'e668d81ed0d85bc6a980be58f6737537469f97700b8d4ab371a39945f81d01a88510e35d0a740bc8ef1388c631182c5defb73a5fda3e99c3b3f7c44641842d5d'
